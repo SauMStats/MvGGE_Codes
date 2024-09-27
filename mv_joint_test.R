337cfca5ba09e98cab310f1dd8f03e204b2605c6
Saurabh Mishra <88341907+SauMStats@users.noreply.github.com>
2024-09-27 10:14:46 +0530

    Update mv_joint_test.R

--- mv_joint_test.R
+++ mv_joint_test.R
@@ -15,3 +15,39 @@
 # 1. Extract genotype data
 # 2. Perform linear regression models for G, GE, and GGE tests
 # 3. Compile p-values from multivariate tests for HDL, LDL, and TG
+
+
+# Load required packages
+required_packages <- c("readr", "data.table", "parallel", "snpStats", "dplyr", "broom")
+install_if_missing <- function(p) {
+  if (!require(p, character.only = TRUE)) {
+    install.packages(p, repos = "http://cran.us.r-project.org")
+    library(p, character.only = TRUE)
+  }
+}
+lapply(required_packages, install_if_missing)
+
+# Load the packages
+library(snpStats)
+library(dplyr)
+library(broom)
+library(parallel)
+library(data.table)
+library(reader)
+
+
+# Load phenotype data
+phenotype_data <- fread(file.path(pheno_dir, "Pheno_Ready.txt"), header = TRUE)
+
+# Function to impute missing genotypes and standardize the matrix
+mat_na <- function(G) {
+  if (any(is.na(G))) {
+    for (i in which(colSums(is.na(G)) != 0)) {
+      G[is.na(G[, i]), i] <- mean(G[, i], na.rm = TRUE)
+    }
+  }
+  scale(G)  # Standardizing the matrix
+}
+
+
+
